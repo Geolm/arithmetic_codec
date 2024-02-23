9e0d6b4be24379dc4fb64807b6060dd51a33f4f2
Geolm <moyaguillaume@gmail.com>
2024-02-23 11:38:40 -0500

    adding unit tests

---
+++ tests/arithmetic_codec.c
@@ -0,0 +1,3 @@
+
+#define __ARITHMETIC_CODEC__IMPLEMENTATION__
+#include "../arithmetic_codec.h"
---
+++ tests/test.c
@@ -0,0 +1,11 @@
+#include "greatest.h"
+
+GREATEST_MAIN_DEFS();
+
+int main(int argc, char **argv)
+{
+    GREATEST_MAIN_BEGIN();
+
+    GREATEST_MAIN_END();
+}
+
