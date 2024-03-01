f122bf33063491e8e0cdd4b5aca1e57d3156ff03
Geolm <moyaguillaume@gmail.com>
2024-03-01 08:56:04 -0500

    added missing licence in the source code

--- arithmetic_codec.h
+++ arithmetic_codec.h
@@ -4,6 +4,17 @@
 // Ported in C from on FastAC by Amir Said's
 // https://github.com/richgel999/FastAC
 
+// The only purpose of this program is to demonstrate the basic principles   -
+// of arithmetic coding. It is provided as is, without any express or        -
+// implied warranty, without even the warranty of fitness for any particular -
+// purpose, or that the implementations are correct.                         -
+//                                                                           -
+// Permission to copy and redistribute this code is hereby granted, provided -
+// that this warning and copyright notices are not removed or altered.       -
+//                                                                           -
+// Copyright (c) 2004 by Amir Said (said@ieee.org) &                         -
+//                       William A. Pearlman (pearlw@ecse.rpi.edu)   
+
 #ifdef __cplusplus
 extern "C" {
 #endif
